6083521b44c22f72fea56169aeb53cddfeebb9ff
Nicholas Armstrong <narmstro@warren.k12.in.us>
2025-01-21 08:45:41 -0500

    lasercan

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,10 +4,12 @@
 
 package frc.robot;
 
+import au.grapplerobotics.LaserCan;
 import com.revrobotics.sim.SparkMaxSim;
 import com.revrobotics.spark.SparkBase;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.ClosedLoopConfig;
+import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.numbers.N1;
@@ -15,29 +17,32 @@ import edu.wpi.first.math.numbers.N2;
 import edu.wpi.first.math.system.LinearSystem;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.units.measure.MutTime;
+import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import au.grapplerobotics.CanBridge;
 
 import static com.revrobotics.spark.SparkLowLevel.*;
+import static edu.wpi.first.units.Units.Seconds;
 
 public class Robot extends TimedRobot {
 
-    private final double JKgMetersSquared = 0.01;
+    private final MutTime lastTime = Seconds.mutable(0.0);
+    private final MutTime currentTime = Seconds.mutable(0.0);
+
+    private final double JKgMetersSquared = 1;
     private final SparkMax motor0 = new SparkMax(16, MotorType.kBrushless);
     private final SparkMax motor1 = new SparkMax(18, MotorType.kBrushless);
-    private final SparkMax motor2 = new SparkMax(24, MotorType.kBrushless);
     private final XboxController controller = new XboxController(0);
 
-    private final Solenoid solenoid =  new Solenoid(PneumaticsModuleType.REVPH, 0);
+    private final Solenoid solenoid1 =  new Solenoid(PneumaticsModuleType.CTREPCM, 4);
+    private final Solenoid solenoid2 = new Solenoid(PneumaticsModuleType.CTREPCM, 5);
+
+
 
     private final SparkMaxSim motor0Sim = new SparkMaxSim(motor0, DCMotor.getNeo550(1));
     private final SparkMaxSim motor1Sim = new SparkMaxSim(motor1, DCMotor.getNeo550(1));
-    private final SparkMaxSim motor2Sim = new SparkMaxSim(motor2, DCMotor.getNeo550(1));
     private final LinearSystem<N2, N1, N2> plant0 = LinearSystemId.createDCMotorSystem(DCMotor.getNeo550(1), JKgMetersSquared, 12.0);
     private final DCMotorSim dcMotorSim0 = new DCMotorSim(plant0, DCMotor.getNeo550(1));
     private final LinearSystem<N2, N1, N2> plant1 = LinearSystemId.createDCMotorSystem(DCMotor.getNeo550(1), JKgMetersSquared, 12.0);
@@ -46,16 +51,20 @@ public class Robot extends TimedRobot {
     private final DCMotorSim dcMotorSim2 = new DCMotorSim(plant0, DCMotor.getNeo550(1));
     private final double deadband = 0.05;
 
+    private final LaserCan laser1 = new LaserCan(9);
+
 
     public Robot() {
+        // CanBridge.runTCP();
         ClosedLoopConfig closedLoopConfig = new SparkMaxConfig()
                 .closedLoop
-                .p(0.1).i(0.0).d(0.0).velocityFF(1.0/11000.0);
+                .p(0.0000000001).i(0.0).d(0.0).velocityFF(1.0/11000.0);
         SparkMaxConfig sparkMaxConfig = new SparkMaxConfig();
+         sparkMaxConfig.idleMode(SparkBaseConfig.IdleMode.kBrake);
         sparkMaxConfig.apply(closedLoopConfig);
-        motor2.configure(sparkMaxConfig, SparkBase.ResetMode.kNoResetSafeParameters, SparkBase.PersistMode.kNoPersistParameters);
         motor1.configure(sparkMaxConfig, SparkBase.ResetMode.kNoResetSafeParameters, SparkBase.PersistMode.kNoPersistParameters);
         motor0.configure(sparkMaxConfig, SparkBase.ResetMode.kNoResetSafeParameters, SparkBase.PersistMode.kNoPersistParameters);
+        lastTime.mut_replace(Timer.getFPGATimestamp(), Seconds);
     }
 
     @Override
@@ -68,46 +77,47 @@ public class Robot extends TimedRobot {
 
         double triggerVoltage = 12.0 * triggerNet;
 
-        motor0.setVoltage(leftValue * 12.0);
-        motor1.setVoltage(rightValue * 12.0);
-        motor2.setVoltage(triggerVoltage);
-
-        
-        if(controller.getAButtonPressed()){
-            solenoid.toggle();
-        }
+        // motor0.setVoltage(leftValue * 12.0);
+        // motor1.setVoltage(rightValue * 12.0);
+        // motor2.setVoltage(triggerVoltage);
 
         // motor0.getClosedLoopController().setReference(leftValue * 11000, SparkBase.ControlType.kVelocity);
-        // motor1.getClosedLoopController().setReference(rightValue * 11000, SparkBase.ControlType.kVelocity);
-        // motor2.getClosedLoopController().setReference(leftValue * 11000, SparkBase.ControlType.kVelocity);
+        // if(laser1.getMeasurement().distance_mm > 10.0){
+        //     motor1.getClosedLoopController().setReference(rightValue * 11000, SparkBase.ControlType.kVelocity);
+        // }else{
+        //     motor1.stopMotor();
+        // }
 
 
-        SmartDashboard.putNumber("Motor 2 Current", motor2.getOutputCurrent());
-        SmartDashboard.putNumber("Motor 2 Velocity RPM", motor2.getEncoder().getVelocity());
+        if(controller.getAButtonPressed()){
+            solenoid1.toggle();
+        }
+
+        if(controller.getBButtonPressed()){
+            solenoid2.toggle();
+        }
+
+        var measure = laser1.getMeasurement();
+        if(measure != null){
+            System.out.println(measure.distance_mm);
+        }else{
+            System.out.println("I'm null for some reason");
+        }
+
+        // SmartDashboard.putNumber("LASERCAN", laser1.getMeasurement().distance_mm);
+        SmartDashboard.putNumber("Motor 0 Current", motor0.getOutputCurrent());
+        SmartDashboard.putNumber("Motor 0 Velocity RPM", motor0.getEncoder().getVelocity());
     }
 
     @Override
     public void simulationPeriodic() {
-        motor0Sim.setAppliedOutput(motor0Sim.getSetpoint());
-        motor0Sim.setBusVoltage(12.0);
-        motor0Sim.setVelocity(dcMotorSim0.getAngularVelocityRPM());
-        motor0Sim.setPosition(dcMotorSim0.getAngularPositionRotations());
-        dcMotorSim0.setInputVoltage(motor0Sim.getSetpoint());
-        dcMotorSim0.update(0.020);
+        currentTime.mut_replace(Timer.getFPGATimestamp(), Seconds);
+        double deltaTSeconds = currentTime.baseUnitMagnitude() - lastTime.baseUnitMagnitude();
+        lastTime.mut_replace(currentTime);
+        dcMotorSim0.setInputVoltage(motor0.getAppliedOutput() * motor0.getBusVoltage());
+        dcMotorSim0.update(deltaTSeconds);
+        motor0Sim.iterate(dcMotorSim0.getAngularVelocityRPM(), 12.0, deltaTSeconds);
 
-        motor1Sim.setAppliedOutput(motor1Sim.getSetpoint());
-        motor1Sim.setBusVoltage(12.0);
-        motor1Sim.setVelocity(dcMotorSim0.getAngularVelocityRPM());
-        motor1Sim.setPosition(dcMotorSim0.getAngularPositionRotations());
-        dcMotorSim1.setInputVoltage(motor1Sim.getSetpoint());
-        dcMotorSim1.update(0.020);
-
-        motor2Sim.setAppliedOutput(motor2Sim.getSetpoint());
-        motor2Sim.setBusVoltage(12.0);
-        motor2Sim.setVelocity(dcMotorSim2.getAngularVelocityRPM());
-        motor2Sim.setPosition(dcMotorSim0.getAngularPositionRotations());
-        dcMotorSim2.setInputVoltage(motor2Sim.getSetpoint());
-        dcMotorSim2.update(0.020);
 
 
     }
